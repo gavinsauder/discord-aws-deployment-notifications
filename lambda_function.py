f05c724054777c70c771b41dc8421738784b1559
Gavin Sauder <40070195+gavinsauder@users.noreply.github.com>
2022-03-01 12:11:42 -0500

    Create lambda_function.py

---
+++ lambda_function.py
@@ -0,0 +1,13 @@
+import json
+import requests
+
+def lambda_handler(event, context):
+
+    message = json.loads(event['Records'][0]['Sns']['Message'])
+
+
+        
+    url = "YOURDISCORDWEBHOOKURL"
+    data = {'username': 'AWS Bot', 'content': message}
+    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+    r = requests.post(url, data=json.dumps(data), headers=headers)
